fbb437e72d0873d4c49672f54c1b3ab1f7bf8d18
mepooh <me.pooh441@gmail.com>
2022-01-15 21:30:11 +0900

    なおしました

--- app/google-calendar-api.go
+++ app/google-calendar-api.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+
+	//"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -75,7 +76,7 @@ func saveToken(path string, token *oauth2.Token) {
 func login() (*calendar.Service, error) {
 	//Google Cloud Platform 上のアプリケーションにアクセスするための情報
 	ctx := context.Background()
-	b, err := ioutil.ReadFile("credentials.json")
+	b, err := os.ReadFile("credentials.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 		return nil, err
@@ -106,4 +107,43 @@ func AddSchedule(event *calendar.Event, id string, srv *calendar.Service) {
 	if err != nil {
 		log.Fatalf("Unable to create event. %v\n", err)
 	}
+}
+
+func main() {
+	service, err := login()
+	if err != nil {
+		log.Fatalf("unable to login : ", err)
+	}
+
+	//ここから予定の追加
+	// Refer to the Go quickstart on how to setup the environment:
+	// https://developers.google.com/calendar/quickstart/go
+	// Change the scope to calendar.CalendarScope and delete any stored credentials.
+
+	event := &calendar.Event{
+		Summary:     "課題があるよ",
+		Location:    "gakujo",
+		Description: "はやくやれ",
+		Start: &calendar.EventDateTime{
+			DateTime: "2022-01-20T09:00:00+09:00",
+			TimeZone: "Asia/Tokyo",
+			//TimeZone: "America/Los_Angeles",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: "2022-01-20T17:00:00+09:00",
+			TimeZone: "Asia/Tokyo",
+			//TimeZone: "America/Los_Angeles",
+		},
+		//Recurrence: []string{"RRULE:FREQ=DAILY;COUNT=2"},
+		/*
+		   Attendees: []*calendar.EventAttendee{
+		   &calendar.EventAttendee{Email:"lpage@example.com"},
+		   &calendar.EventAttendee{Email:"sbrin@example.com"},
+		   },
+		*/
+	}
+
+	calendarId := "primary"
+
+	AddSchedule(event, calendarId, service)
 }
