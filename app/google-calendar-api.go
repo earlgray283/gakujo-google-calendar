2a9fcb3b3a9b375ed71862945e70d0390534ea92
mepooh <me.pooh441@gmail.com>
2022-01-15 20:21:14 +0900

    進捗あります

--- app/google-calendar-api.go
+++ app/google-calendar-api.go
@@ -1,3 +1,147 @@
 package app
 
-// google カレンダー api
+import (
+        "context"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        //"time"
+
+        "golang.org/x/oauth2"
+        "golang.org/x/oauth2/google"
+        "google.golang.org/api/calendar/v3"
+        "google.golang.org/api/option"
+)
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        tokFile := "token.json"
+        tok, err := tokenFromFile(tokFile)
+        if err != nil {
+                tok = getTokenFromWeb(config)
+                saveToken(tokFile, tok)
+        }
+        return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+        fmt.Printf("Go to the following link in your browser then type the "+
+                "authorization code: \n%v\n", authURL)
+
+        var authCode string
+        if _, err := fmt.Scan(&authCode); err != nil {
+                log.Fatalf("Unable to read authorization code: %v", err)
+        }
+
+        tok, err := config.Exchange(context.TODO(), authCode)
+        if err != nil {
+                log.Fatalf("Unable to retrieve token from web: %v", err)
+        }
+        return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+        f, err := os.Open(file)
+        if err != nil {
+                return nil, err
+        }
+        defer f.Close()
+        tok := &oauth2.Token{}
+        err = json.NewDecoder(f).Decode(tok)
+        return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+        fmt.Printf("Saving credential file to: %s\n", path)
+        f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+        if err != nil {
+                log.Fatalf("Unable to cache oauth token: %v", err)
+        }
+        defer f.Close()
+        json.NewEncoder(f).Encode(token)
+}
+
+func login() (*calendar.Service, error){
+        //Google Cloud Platform 上のアプリケーションにアクセスするための情報
+        ctx := context.Background()
+        b, err := ioutil.ReadFile("credentials.json")
+        if err != nil {
+                log.Fatalf("Unable to read client secret file: %v", err)
+                return nil, err
+        }
+
+        //スコープの設定
+        // If modifying these scopes, delete your previously saved token.json.
+        //config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+        config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+        if err != nil {
+                log.Fatalf("Unable to parse client secret file to config: %v", err)
+                return nil, err
+        }
+        client := getClient(config)
+
+        srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+        if err != nil {
+                log.Fatalf("Unable to retrieve Calendar client: %v", err)
+                return nil, err
+        }
+
+        return srv, err
+}
+
+func AddSchedule(event *calendar.Event, id string) {
+        srv, err := login()
+        if err != nil {
+                log.Fatalf("unable to login : ", err)
+        }
+        event, err = srv.Events.Insert(id, event).Do()
+        if err != nil {
+                log.Fatalf("Unable to create event. %v\n", err) 
+        }
+}
+
+func main() {
+
+        //ここから予定の追加
+        // Refer to the Go quickstart on how to setup the environment:
+        // https://developers.google.com/calendar/quickstart/go
+        // Change the scope to calendar.CalendarScope and delete any stored credentials.
+
+        event := &calendar.Event{
+                Summary: "Google I/O 2015",
+                Location: "800 Howard St., San Francisco, CA 94103",
+                Description: "A chance to hear more about Google's developer products.",
+                Start: &calendar.EventDateTime{
+                DateTime: "2022-01-15T09:00:00+09:00",
+                TimeZone: "Asia/Tokyo",
+                //TimeZone: "America/Los_Angeles",
+                },
+                End: &calendar.EventDateTime{
+                DateTime: "2022-01-15T17:00:00+09:00",
+                TimeZone: "Asia/Tokyo",
+                //TimeZone: "America/Los_Angeles",
+                },
+                //Recurrence: []string{"RRULE:FREQ=DAILY;COUNT=2"},
+                /*
+                Attendees: []*calendar.EventAttendee{
+                &calendar.EventAttendee{Email:"lpage@example.com"},
+                &calendar.EventAttendee{Email:"sbrin@example.com"},
+                },
+                */
+        }
+        
+        calendarId := "primary"
+        
+        AddSchedule(event, calendarId)
+      
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/earlgray283/gakujo-google-calendar
 
 go 1.17
+
+require (
+	cloud.google.com/go/compute v1.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.65.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220114231437-d2e6a121cae0 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
