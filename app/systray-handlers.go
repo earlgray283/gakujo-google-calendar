af0a8e7937f240edd774b7dffc9fad07c68907c2
Seele0709 <yu07090618@gmail.com>
2022-01-21 07:27:24 +0900

    各種修正しました

--- app/systray-handlers.go
+++ app/systray-handlers.go
@@ -12,8 +12,8 @@ import (
 	"google.golang.org/api/calendar/v3"
 )
 
-const dateTimeNotSubmited = "0001-01-01 00:00:00 +0000 UTC" //未提出の課題を選別するめのconst
-const formatAdded = "2006-01-02 15:04.05"
+const dateTimeNotSubmited = "0001-01-01 00:00:00 +0000 UTC" //未提出の課題を選別するためのconst
+const addedAt = "2006-01-02T15:04:05+09:00"
 const googleCalendarURL = "https://calendar.google.com/calendar/" //GoogleCalendarのURL(string参照)
 
 func (a *App) OnReady() {
@@ -29,14 +29,14 @@ func (a *App) OnReady() {
 	systray.SetTooltip("Set tasks to your google calendar automaticaly")
 
 	//GoogleCalendarをWebSiteで開くためのボタン
-	WebSiteOpener := systray.AddMenuItem("Open Google Calendar in Web Site", "Open Google Calendar")
+	webSiteOpener := systray.AddMenuItem("Open Google Calendar in Web Site", "Open Google Calendar")
 	systray.AddSeparator()
 
 	// GoogleCalenderに登録するためのボタンの作成
-	AllAdder := systray.AddMenuItem("Add every task to calendar", "ADD all")
-	ReportAdder := systray.AddMenuItem("Add report to calendar", "ADD report")
-	MinitestAdder := systray.AddMenuItem("Add minitest to calendar", "ADD minitest")
-	ClassEnqAdder := systray.AddMenuItem("Add classenq to calendar", "ADD classenq")
+	allAdder := systray.AddMenuItem("Add every task to calendar", "ADD all")
+	reportAdder := systray.AddMenuItem("Add report to calendar", "ADD report")
+	minitestAdder := systray.AddMenuItem("Add minitest to calendar", "ADD minitest")
+	classEnqAdder := systray.AddMenuItem("Add classenq to calendar", "ADD classenq")
 
 	// 登録と退出の境界線を作る
 	systray.AddSeparator()
@@ -45,8 +45,7 @@ func (a *App) OnReady() {
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 
 	// 定期実行する
-	err = a.autoAddSchedule()
-	if err != nil {
+	if err = a.autoAddSchedule(); err != nil {
 		a.Log.Println(err)
 		a.Log.Println("定期実行に失敗しました")
 		systray.Quit()
@@ -54,15 +53,14 @@ func (a *App) OnReady() {
 
 	for {
 		select {
-		case <-WebSiteOpener.ClickedCh:
-			err := a.openWebSite(googleCalendarURL)
-			if err != nil {
+		case <-webSiteOpener.ClickedCh:
+			if err := a.openWebSite(googleCalendarURL); err != nil {
 				a.Log.Println(err)
 				a.Log.Println("WebSiteにアクセスできませんでした")
 				systray.Quit()
 			}
 			a.Log.Println("Google Calendarを開きます。")
-		case <-AllAdder.ClickedCh:
+		case <-allAdder.ClickedCh:
 			count, err := a.registAll()
 			if err != nil {
 				a.Log.Println(err)
@@ -76,7 +74,7 @@ func (a *App) OnReady() {
 				a.Log.Println("登録する課題はありませんでした。")
 			}
 
-		case <-ReportAdder.ClickedCh:
+		case <-reportAdder.ClickedCh:
 			count, err := a.registReport()
 			if err != nil {
 				a.Log.Println(err)
@@ -90,7 +88,7 @@ func (a *App) OnReady() {
 				a.Log.Println("登録するレポート課題はありませんでした。")
 			}
 
-		case <-MinitestAdder.ClickedCh:
+		case <-minitestAdder.ClickedCh:
 			count, err := a.registMinitest()
 			if err != nil {
 				a.Log.Println(err)
@@ -104,7 +102,7 @@ func (a *App) OnReady() {
 				a.Log.Println("登録する小テスト課題はありませんでした。")
 			}
 
-		case <-ClassEnqAdder.ClickedCh:
+		case <-classEnqAdder.ClickedCh:
 			count, err := a.registClassEnq()
 			if err != nil {
 				a.Log.Println(err)
@@ -132,11 +130,7 @@ func (a *App) OnExit() {
 }
 
 func (a *App) openWebSite(url string) error {
-	err := open.Run(url)
-	if err != nil {
-		return err
-	}
-	return nil
+	return open.Run(url)
 }
 
 func (a *App) registReport() (int, error) {
@@ -148,8 +142,7 @@ func (a *App) registReport() (int, error) {
 
 		// 未提出だったら・・・・・・・・・・・
 		if row.LastSubmitDate.String() == dateTimeNotSubmited {
-			err := AddSchedule(event, calendarId, a.srv)
-			if err != nil {
+			if err := AddSchedule(event, calendarId, a.srv); err != nil {
 				return -1, err
 			}
 			counter += 1
@@ -167,8 +160,7 @@ func (a *App) registMinitest() (int, error) {
 
 		if row.SubmitStatus == "未提出" {
 			if time.Now().Before(row.EndDate) {
-				err := AddSchedule(event, calendarId, a.srv)
-				if err != nil {
+				if err := AddSchedule(event, calendarId, a.srv); err != nil {
 					return -1, err
 				}
 				counter += 1
@@ -189,8 +181,7 @@ func (a *App) registClassEnq() (int, error) {
 		// 未提出だったら・・・・・・・・・・・
 		if row.SubmitStatus == "未提出" {
 			if time.Now().Before(row.EndDate) {
-				err := AddSchedule(event, calendarId, a.srv)
-				if err != nil {
+				if err := AddSchedule(event, calendarId, a.srv); err != nil {
 					return -1, nil
 				}
 				counter += 1
@@ -225,17 +216,17 @@ func (a *App) registAll() (int, error) {
 }
 
 func newEvent(title string, t time.Time) *calendar.Event { // タイトルと日時を入れると Event 型を返す
-	added := time.Now().Format(formatAdded)
+	added := time.Now().Format(addedAt)
 	Event := &calendar.Event{
 		Summary:     title,
 		Location:    "学務情報システム",
 		Description: "学情カレンダーから追加された予定です。\n学務情報システム: https://gakujo.shizuoka.ac.jp/portal/\nAdded: " + added,
 		Start: &calendar.EventDateTime{
-			DateTime: t.Add(-time.Hour).Format(formatAdded),
+			DateTime: t.Add(-time.Hour).Format(addedAt),
 			TimeZone: "Asia/Tokyo",
 		},
 		End: &calendar.EventDateTime{
-			DateTime: t.Format(formatAdded),
+			DateTime: t.Format(addedAt),
 			TimeZone: "Asia/Tokyo",
 		},
 	}
