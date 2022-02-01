0d4f6062b00ad1bcd8b85bd702501be82590d5ab
earlgray <earlgray283@gmail.com>
2022-02-01 12:28:24 +0900

    fix: main のログをファイルに吐くようにした

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	_ "embed"
-	"log"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/earlgray283/gakujo-google-calendar/app"
 	"github.com/earlgray283/gakujo-google-calendar/app/crawle"
@@ -10,37 +12,56 @@ import (
 	"github.com/earlgray283/gakujo-google-calendar/app/util"
 )
 
-//go:embed credentials.json
-var CredentialsJsonByte []byte
+var (
+	//go:embed credentials.json
+	CredentialsJsonByte []byte
+
+	logDirPath    string
+	configDirPath string
+	logFile       *os.File
+)
+
+func init() {
+	logDirPath = util.DefaultCacheDir()
+	configDirPath = util.DefaultConfigDir()
+	logFile, _ = os.Create(filepath.Join(logDirPath, "log.txt"))
+}
 
 func main() {
-	config, err := app.LoadConfig(util.DefaultConfigDir())
+	defer logFile.Close()
+
+	config, err := app.LoadConfig(configDirPath)
 	if err != nil {
 		config, err = app.GenerateConfig(CredentialsJsonByte)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintln(logFile, err)
+			return
 		}
-		if err := app.SaveConfig(config, util.DefaultConfigDir()); err != nil {
-			log.Fatal(err)
+		if err := app.SaveConfig(config, configDirPath); err != nil {
+			fmt.Fprintln(logFile, err)
+			return
 		}
 	}
 
 	crawler, err := crawle.NewCrawler(config.Username, config.Password, crawle.DefaultCrawleOption())
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(logFile, err)
+		return
 	}
 
 	srv, err := calendar.NewService(CredentialsJsonByte, config.Token)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(logFile, err)
+		return
 	}
 
-	app, err := app.NewApp(crawler, srv)
+	app, err := app.NewApp(crawler, srv, logDirPath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(logFile, err)
+		return
 	}
 	if err := app.Run(); err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(logFile, err)
+		return
 	}
-
 }
